90776c00981bfa2509014d8a382a4660adf8855a
Marek <Marek.Myjak1221@gmail.com>
2019-03-20 18:23:50 +0100

    Add project

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @Autowired
+    private Human human;
+
+    @GetMapping("hello")
+    public String hello(){
+
+        return "some text";
+    }
+}
---
+++ src/main/java/com/example/demo/Human.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Human {
+
+    public Human() {
+
+    }
+}
